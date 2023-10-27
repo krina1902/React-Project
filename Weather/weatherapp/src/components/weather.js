e241fb8251e7b581e57d4e8b332803618a0146a1
krina1902 <krinaantala33@gmail.com>
2023-10-27 10:10:01 +0530

    react

--- Weather/weatherapp/src/components/weather.js
+++ Weather/weatherapp/src/components/weather.js
@@ -1,13 +1,44 @@
-import React from 'react';
-// import './styles.css';
-import { Card } from 'semantic-ui-react'
+import React, { useState,useEffect } from "react";
+import Displayweather from "./Displayweather";
 
-const CardExampleCard = ({weatherData}) => (
-  <Card>
-    <Card.Content>
-        <Card.Header className="header">{weatherData.name}</Card.Header>
-    </Card.Content>
-  </Card>
-)
+function Weather () {
 
-export default CardExampleCard;
+    const APIKEY ='1a57238edcbeaff87a7c5f08fca39278'
+
+    const [lat, setLat] = useState([]);
+    const [long, setLong] = useState([]);
+
+    const [data,setData] = useState([])
+
+    useEffect(() => {
+        navigator.geolocation.getCurrentPosition(function(position) {
+          setLat(position.coords.latitude);
+          setLong(position.coords.longitude);
+        });
+    
+        console.log("Latitude is:", lat)
+        console.log("Longitude is:", long)
+
+        const data =  fetch(`https://api.openweathermap.org/data/2.5/weather/?lat=${lat}&lon=${long}&units=metric&APPID=${APIKEY}`).then((res) => res.json()).then((data) => {
+        setData(data)    
+        console.log(data)})
+        
+      }, [lat, long]);
+
+    return ( 
+        <>
+        <div className="App">
+      {(typeof data.main != 'undefined') ? (
+        <Displayweather weatherData={data}/>
+      ): (
+        <div></div>
+      )}
+      
+    </div>
+            
+
+        </>
+     );
+}
+ 
+export default Weather;
