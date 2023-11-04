647d95c58a65e2bc9c6aec177a12086a673b6223
krina1902 <krinaantala33@gmail.com>
2023-11-04 10:11:33 +0530

    react

--- React homepage ass/homeapp/src/Practice/Animation.jsx
+++ React homepage ass/homeapp/src/Practice/Animation.jsx
@@ -1,18 +1,19 @@
 import { Fade,Zoom,Jump } from "react-swift-reveal";
+import "./ani.css"
 
 const Animation = () => {
   return (
     <>
-    <Fade>
+    <Fade timeout={10000} count={5}>
       <h1>hello world</h1>
     </Fade>
 
-    <Zoom>
-    <h1>hello world</h1>
+    <Zoom timeout={10000} count={5}>
+    <h1 className="squ"></h1>
     </Zoom>
 
-    <Jump>
-    <h1>hello world</h1>
+    <Jump timeout={1000} forever duration={10000}>
+    <h1 className="box"></h1>
     </Jump>
 </>
   );
